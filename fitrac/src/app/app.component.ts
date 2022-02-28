2e582f64876c39a425656212cf049850c1c8a702
wmujahid <wmujahid47@gmail.com>
2022-02-28 14:33:57 -0500

    added getAllBudgets function in app.component.ts - changed app.component to implement OnInit

--- fitrac/src/app/app.component.ts
+++ fitrac/src/app/app.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BudgetService } from './budget.service';
 import { Budget } from './budget';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   budgets: Budget[] = [];
 
   constructor(private budgetService: BudgetService) { }
+
+  ngOnInit(): void {
+    this.getAllBudgets()
+  }
+  getAllBudgets(): void {
+    this.budgetService.getAllBudgets().subscribe(
+      (response: Budget[]) => {
+        this.budgets = response
+      },
+      (err: HttpErrorResponse) => {
+        alert(err.message)
+      }
+    )
+  }
 }
