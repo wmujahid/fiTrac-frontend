d617c42779d8aa7ef202ac7b2ed39d718722f4a1
wmujahid <wmujahid47@gmail.com>
2022-02-28 13:48:45 -0500

    removed boilerplate code

--- fitrac/src/app/app.module.ts
+++ fitrac/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Budget } from './budget.ts/budget.ts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Budget.TsComponent
   ],
   imports: [
     BrowserModule,
