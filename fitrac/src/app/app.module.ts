4e5173d18d2a84c771ea85b308902df7e8e8395b
wmujahid <wmujahid47@gmail.com>
2022-02-28 19:31:49 -0500

    added button and input fields for amount and description

--- fitrac/src/app/app.module.ts
+++ fitrac/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { BudgetService } from './budget.service';
 import { MainPageComponent } from './main-page/main-page.component';
 import { AddItemFormComponent } from './add-item-form/add-item-form.component';
+import { NO_ERRORS_SCHEMA } from '@angular/compiler';
 
 @NgModule({
   declarations: [
